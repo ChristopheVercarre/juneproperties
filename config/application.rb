3a8fc61c591ec6956cb43d10199609f7106557a3
Hassan Mahmood <devhassan.mahmood@gmail.com>
2020-09-17 18:05:23 +0500

    application.rb fixed

--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,20 @@ Bundler.require(*Rails.groups)
 
 module Juneproperties
   class Application < Rails::Application
+    config.generators do |generate|
+      generate.assets false
+      generate.helper false
+      generate.test_framework :rspec, fixture: false
+      generate.factory_bot dir: '/spec/factories/'
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.paths << Rails.root.join('app/assets/fonts')
+    config.eager_load_paths << Rails.root.join('lib')
+    # config.autoload_paths += Dir[Rails.root.join('app/models/*/')]
   end
 end
